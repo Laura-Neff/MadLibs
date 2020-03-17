246230bd87b373c0007eb8454e7b45b339a97615
Laura Neff <40323885+Laura-Neff@users.noreply.github.com>
2020-03-17 12:32:25 -0400

    Add files via upload

---
+++ MadLibsGame.py
@@ -0,0 +1,8 @@
+
+
+
+color = input("Enter color: ")
+person = input("Enter your favorite person: ")
+
+print("His hair was the loveliest shade of " + color)
+print("And that man standing at my door step was " + person)
